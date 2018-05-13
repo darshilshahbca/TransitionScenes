0d30a7566fabd2246b312d684e475fcf814924cc
darshilshahbca <darshil.saphcm@gmail.com>
2018-05-13 15:25:14 +0530

    Define and run a Transition

--- app/src/main/java/com/example/david/transitions/MainActivity.java
+++ app/src/main/java/com/example/david/transitions/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.david.transitions;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Scene;
+import android.transition.TransitionInflater;
+import android.transition.TransitionManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    int mCurrentScene = 1;
+    private Scene mScene1, mScene2;
+    private ViewGroup mSceneRoot;
+    private TransitionManager mManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSceneRoot = (ViewGroup) findViewById(R.id.scene_root);
+        mScene1 = Scene.getSceneForLayout(mSceneRoot, R.layout.scene1, this);
+        mScene2 = Scene.getSceneForLayout(mSceneRoot, R.layout.scene2, this);
+        mManager = TransitionInflater.from(this)
+                    .inflateTransitionManager(R.transition.manager, mSceneRoot);
 
     }
 
@@ -23,9 +39,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_transition) {
+            runTransition();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void runTransition() {
+        if(mCurrentScene == 1){
+            TransitionManager.go(mScene2);
+            mCurrentScene = 2;
+        } else{
+            mManager.transitionTo(mScene1);
+            mCurrentScene = 1;
+        }
     }
 
 }
